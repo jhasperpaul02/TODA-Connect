d6b7a8592deb21343e0b0de7d5037435896b24ac
TODA Fare <166193594+jhasperpaul02@users.noreply.github.com>
2025-09-14 21:22:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,82 @@
-const wrapper = document.querySelector('.wrapper');
-const registerLink = document.querySelector('.register-link');
-const loginLink = document.querySelector('.login-link');
-
-registerLink.onclick = () => {
-    wrapper.classList.add('active');
+body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Arial', sans-serif;
+  background: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 }
 
-loginLink.onclick = () => {
-    wrapper.classList.remove('active')
+.container {
+  text-align: center;
+}
+
+.title {
+  font-size: 28px;
+  font-weight: bold;
+  margin-bottom: 20px;
+}
+
+.login-box {
+  background: #182020; /* dark background */
+  padding: 40px 30px;
+  border-radius: 20px;
+  color: white;
+  width: 90%;
+  max-width: 300px;
+  box-shadow: 0px 4px 10px rgba(0,0,0,0.2);
+}
+
+.driver-check{
+  margin-left: -29px;
+}
+
+.login-box h2 {
+  font-size: 26px;
+  margin-bottom: 30px;
+}
+
+.input-box {
+  margin-bottom: 30px;
+  text-align: right;
+  margin-right: 10px;
+}
+
+.input-box label {
+  display: block;
+  font-size: 14px;
+  margin-bottom: 5px;
+}
+
+.input-box input {
+  width: 100%;
+  padding: 10px;
+  border: none;
+  border-bottom: 1px solid white;
+  background: transparent;
+  color: white;
+  font-size: 14px;
+  outline: none;
+}
+
+.btn {
+  display: block;
+  width: 100%;
+  background: #00e5ff;
+  border: none;
+  padding: 12px;
+  margin: 10px 0;
+  border-radius: 25px;
+  font-size: 16px;
+  font-weight: bold;
+  cursor: pointer;
+  color: black;
+  transition: background 0.3s;
+}
+
+.btn:hover {
+  background: #04666e;
+  transition: 1s;
 }
